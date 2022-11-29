d630bf1a158409b7c68e6b1d4ec4d04cfe8e5fcf
Matheus Laurindo <matheus104521@gmail.com>
2022-11-29 19:42:53 -0300

    chore: add eslint e prettier

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
