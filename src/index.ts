1bc62fd1c39fe5d88b7fb450e49633136aaa4328
Matheus Laurindo <matheus104521@gmail.com>
2022-11-29 21:06:30 -0300

    feat: primeira rota

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
 app.use(express.json());
 
 const PORT = 3000;
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World!');
+});
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
